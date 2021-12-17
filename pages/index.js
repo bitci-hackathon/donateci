256315a882a3322944f0f689d11759170aff2561
Emrah ÖZDEMİR <owner@emrahozdemr.com>
2021-12-17 23:26:55 +0300

    search + listing item

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,53 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/outline'
 import Layout from '../components/common/layout'
+import ListingItem from '../components/listing/item'
+
+
+const listings = [
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 2,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  // More products...
+]
 
 export default function Home() {
   return (
     <Layout>
-        Test
+      <>
+        <form className="group sticky top-0 z-50 bg-white px-4 sm:px-6 lg:px-10 shadow">
+        <div className="max-w-10xl mx-auto flex">
+          <label htmlFor="search-input" className="flex-none pr-2 flex items-center">
+            <span className="sr-only">Search all Creators</span>
+            <SearchIcon  className="h-6 w-6" aria-hidden="true"></SearchIcon>
+          </label>
+          <input type="text" id="search-input" value="" placeholder="Search all Creators" 
+          className="flex-auto py-4 text-base leading-6 text-gray-500 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400"/>
+        </div>
+        </form>
+      </>
+    
+      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+
+        {listings.map((listing) => (
+          <ListingItem key={listing.id} listing={listing} />
+        ))}
+
+      </div>
     </Layout>
   )
 }
