26d4de6b914da448fd9398352ecb8d01b855e8e1
Xia <alperbagdemir@gmail.com>
2021-12-19 18:44:47 +0300

    create nft

---
+++ pages/create-nft/index.js
@@ -0,0 +1,56 @@
+import { ArrowCircleDownIcon, ArrowCircleUpIcon } from "@heroicons/react/outline";
+import { useWeb3React } from "@web3-react/core";
+import { useState } from "react";
+import Account from "../../components/Account";
+import Layout from "../../components/common/layout";
+import TokenSelect from "../../components/swap/token-select";
+import useEagerConnect from "../../hooks/useEagerConnect";
+
+export default function Swap() {
+    const [rotate,setRotate] = useState(true);
+    const { active, account, library, connector, activate, deactivate } =    useWeb3React();
+    const isConnected = typeof account === "string" && !!library;
+    const triedEagerConnect = useEagerConnect();
+
+    return (
+        <Layout>
+
+            <div className="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden">
+                <div className="px-4 py-8 sm:px-10">
+                   
+                    <div className="">
+                        <div className="w-full space-y-6">
+
+                            <div className="w-full">
+                                <div className=" relative ">
+                                    <input type="text" id="search-form-location" 
+                                    className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+                                    placeholder="0.0" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="block w-full rounded-md shadow-sm">
+                                    {isConnected ? (
+                                        <button type="button" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                            Convert
+                                        </button>
+                                    ): (
+                                        <Account triedToEagerConnect={triedEagerConnect} />
+                                    )}
+                                    
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="px-4 py-6 border-t-2 border-gray-200 bg-gray-50 sm:px-10">
+                    <p className="text-xs leading-5 text-gray-500">
+                        Info text.
+                    </p>
+                </div>
+            </div>
+
+        </Layout>
+    )
+}
