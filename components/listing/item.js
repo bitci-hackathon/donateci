07d3becd0ffd0f9d09ced6f63a8d9a7c4e4e8fed
Xia <alperbagdemir@gmail.com>
2021-12-19 08:49:13 +0300

    listing

--- components/listing/item.js
+++ components/listing/item.js
@@ -1,5 +1,11 @@
 
 const ListingItem = ({ listing }) => {
+
+    const str = listing.id.toString();
+    const begin = str.substring(0,8);
+    const end = str.substring(str.length-8,str.length);
+    const id = begin +"......" + end;
+
     return (
         <div className="group relative">
             <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
@@ -8,6 +14,9 @@ const ListingItem = ({ listing }) => {
                     alt={listing.id}
                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
+            </div>
+            <div className="flex w-full justify-center relative ">
+                <h1 className="text-center w-10/12 -mt-14 flex  items-center justify-center text-white text-xs overflow-ellipsis hover:font-bold z-50 hover:cursor-pointer">{id}</h1>
             </div>
             <div className="mt-1 flex justify-between">
                 <div className="bg-gray-800 w-full p-2 rounded text-center  font-medium">
